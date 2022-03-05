e76808dc94b50614ba041c3fa9122ec614af4bc0
EC2 Default User <ec2-user@ip-172-31-8-245.ec2.internal>
2022-03-05 13:13:42 +0000

    [add] 開発環境ファイルにホスト許可を追記

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,6 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.hosts << "204f5f244c2f4a3d973c33ca02a77f6e.vfs.cloud9.us-east-1.amazonaws.com"
+
 end
